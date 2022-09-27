3a85d88dd1138ae5627b279a19102c0251507d35
samhita20 <gsamhita123@gmail.com>
2022-09-27 22:03:51 +0530

    fourth

--- src/components/EditPreferences.jsx
+++ src/components/EditPreferences.jsx
@@ -1,69 +1,141 @@
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect, Component} from "react";
+import axios from 'axios';
 
-function EditPreferences(props) {
-    const bugid = parseInt(props.bugid);
+class EditPreferences extends Component {
 
-    const [disableButton, setDisableButton] = useState(true);
+    state = {
+        isId: false,
+        isType: false,
+        isStatus: false,
+        isSummary: false,
+        isProduct: false,
+        isSeverity: false,
+        isPriority: false,
+        isLastChangeTime: false,
+        isAssignedToDetails: false,
+        isCreatorDetails: false,
+        isCreationTime: false,
+        isQaContactDetails: false,
+    };
 
-    const bugInformation = {
-        "ID": 0,
-        "CreatedAt": "2022-09-05T22:20:46.968199+05:30",
-        "UpdatedAt": "2022-09-22T10:45:36.747303+05:30",
-        "DeletedAt": null,
-        "Id": 34620,
-        "Comment_count": 151,
-        "Type": "VELOS",
-        "Status": "VERIFIED",
-        "Priority": "P3",
-        "Summary": "address book ",
-        "Severity": "normal",
-        "Product": "MailNews Core",
-        "Platform": "Other",
-        "Resolution": "FIXED",
-        "Target_milestone": "M16",
-        "Classification": "Components",
-        "Is_confirmed": true,
-        "Is_open": false,
-        "Qa_contactID": 4114,
-        "Creator_detailID": 14096,
-        "Assigned_to_detailID": 4465,
-        "Qa_contact": {
-          "ID": 0,
-          "CreatedAt": "2022-09-05T22:20:46.952779+05:30",
-          "UpdatedAt": "2022-09-05T22:20:46.952779+05:30",
-          "DeletedAt": null,
-          "Id": 4114,
-          "Real_name": "",
-          "Name": "esther@formerly-netscape.com.tld",
-          "Email": "esther@formerly-netscape.com.tld",
-          "Nick": "esther"
-        },
-        "Creator_detail": {
-          "ID": 0,
-          "CreatedAt": "2022-09-05T22:20:46.956671+05:30",
-          "UpdatedAt": "2022-09-05T22:20:46.956671+05:30",
-          "DeletedAt": null,
-          "Id": 14096,
-          "Real_name": "",
-          "Name": "dragon@gocougs.wsu.edu",
-          "Email": "dragon@gocougs.wsu.edu",
-          "Nick": "dragon"
-        },
-        "Assigned_to_detail": {
-          "ID": 0,
-          "CreatedAt": "2022-09-05T22:20:46.96076+05:30",
-          "UpdatedAt": "2022-09-05T22:20:46.96076+05:30",
-          "DeletedAt": null,
-          "Id": 4465,
-          "Real_name": "Jean-Francois Ducarroz",
-          "Name": "bugzilla@ducarroz.org",
-          "Email": "bugzilla@ducarroz.org",
-          "Nick": "bugzilla"
-        },
-        "Last_change_time": "2008-07-31T13:52:10+05:30",
-        "Creation_time": "2000-04-06T00:16:02+05:30"
-      };
+    onChangeID = () => {
+        this.setState(initialState => ({
+          isId: !initialState.isId,
+        }));
+    }
 
+    onChangeType = () => {
+        this.setState(initialState => ({
+          isType: !initialState.isType,
+        }));
+    }
+
+    onChangeStatus = () => {
+        this.setState(initialState => ({
+          isStatus: !initialState.isStatus,
+        }));
+    }
+
+    onChangeSummary = () => {
+        this.setState(initialState => ({
+          isSummary: !initialState.isSummary,
+        }));
+    }
+
+    onChangeProduct = () => {
+        this.setState(initialState => ({
+          isProduct: !initialState.isProduct,
+        }));
+    }
+
+    onChangeSeverity = () => {
+        this.setState(initialState => ({
+          isSeverity: !initialState.isSeverity,
+        }));
+    }
+
+    onChangePriority = () => {
+        this.setState(initialState => ({
+          isPriority: !initialState.isPriority,
+        }));
+    }
+
+    onChangeLastChangeTime = () => {
+        this.setState(initialState => ({
+          isLastChangeTime: !initialState.isLastChangeTime,
+        }));
+    }
+
+    onChangeAssignedToDetails = () => {
+        this.setState(initialState => ({
+          isAssignedToDetails: !initialState.isAssignedToDetails,
+        }));
+    }
+
+    onChangeAssignedToDetails = () => {
+        this.setState(initialState => ({
+          isAssignedToDetails: !initialState.isAssignedToDetails,
+        }));
+    }
+
+    onChangeCreatorDetails = () => {
+        this.setState(initialState => ({
+          isCreatorDetails: !initialState.isCreatorDetails,
+        }));
+    }
+
+    onChangeCreationTime = () => {
+        this.setState(initialState => ({
+          isCreationTime: !initialState.isCreationTime,
+        }));
+    }
+
+    onChangeQaContactDetails = () => {
+        this.setState(initialState => ({
+          isQaContactDetails: !initialState.isQaContactDetails,
+        }));
+    }
+
+    // onSubmit = (e) => {
+    //     e.preventDefault();
+    //     console.log(this.state);
+    // }
+    
+    // onSubmit = (e) => {
+    //     e.preventDefault();
+    //     let checkArray = [];
+    //     for (var key in this.state) {
+    //       if (this.state[key] === true) {
+    //         checkArray.push(key);
+    //       }
+    //     }
+    //     let checkData = {
+    //       checkbox: checkArray.toString()
+    //     };
+    //     axios.post(checkData)
+    //       .then((res) => {
+    //         console.log(res.data)
+    //       }).catch((error) => {
+    //         console.log(error)
+    //       });
+    //   }
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        let checkArray = [];
+        for (var key in this.state) {
+          if (this.state[key] === true) {
+            checkArray.push(key);
+          }
+        }
+        let checkData = {
+          checkbox: checkArray.toString()
+        };
+
+        console.log(this.state);
+      }
+
+    render () {
 
     return (
         <div>
@@ -72,24 +144,28 @@ function EditPreferences(props) {
                 <br/>
                 <br/>
                 <br/>
+
+                <br/>
+                <br/>
             </div>
 
+            <form onSubmit={this.onSubmit}>
             <div className="row">
                 <div className="col-lg-4 col-md-4 col-sm-4 spacing">
                     <div class="form-check form-switch">
-                        <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault" />
+                        <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault" checked={this.state.isId} onChange={this.onChangeId}/>
                         <label class="form-check-label" for="flexSwitchCheckDefault">ID</label>
                     </div>
                 </div>
                 <div className="col-lg-4 col-md-4 col-sm-4 spacing">
                     <div class="form-check form-switch">
-                        <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault" />
+                        <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault" checked={this.state.isType} onChange={this.onChangeType} />
                         <label class="form-check-label" for="flexSwitchCheckDefault">Type</label>
                     </div>
                 </div>
                 <div className="col-lg-3 col-md-4 col-sm-4">
                     <div class="form-check form-switch">
-                        <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault" />
+                        <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault" checked={this.state.isStatus} onChange={this.onChangeStatus} />
                         <label class="form-check-label" for="flexSwitchCheckDefault">Status</label>
                     </div>
                 </div>
@@ -103,19 +179,19 @@ function EditPreferences(props) {
             <div className="row">
                 <div className="col-lg-4 col-md-4 col-sm-4 spacing">
                     <div class="form-check form-switch">
-                        <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault" />
+                        <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault" checked={this.state.isSummary} onChange={this.onChangeSummary} />
                         <label class="form-check-label" for="flexSwitchCheckDefault">Summary</label>
                     </div>
                 </div>
                 <div className="col-lg-4 col-md-4 col-sm-4 spacing">
                     <div class="form-check form-switch">
-                        <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault" />
+                        <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault" checked={this.state.isProduct} onChange={this.onChangeProduct} />
                         <label class="form-check-label" for="flexSwitchCheckDefault">Product</label>
                     </div>
                 </div>
                 <div className="col-lg-3 col-md-4 col-sm-4">
                     <div class="form-check form-switch">
-                        <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault" />
+                        <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault" checked={this.state.isSeverity} onChange={this.onChangeSeverity} />
                         <label class="form-check-label" for="flexSwitchCheckDefault">Severity</label>
                     </div>
                 </div>
@@ -129,19 +205,19 @@ function EditPreferences(props) {
             <div className="row">
                 <div className="col-lg-4 col-md-4 col-sm-4 spacing">
                     <div class="form-check form-switch">
-                        <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault" />
+                        <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault" checked={this.state.isPriority} onChange={this.onChangePriority} />
                         <label class="form-check-label" for="flexSwitchCheckDefault">Priority</label>
                     </div>
                 </div>
                 <div className="col-lg-4 col-md-4 col-sm-4 spacing">
                     <div class="form-check form-switch">
-                        <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault" />
+                        <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault" checked={this.state.isLastChangeTime} onChange={this.onChangeLastChangeTime} />
                         <label class="form-check-label" for="flexSwitchCheckDefault">Last Change Time</label>
                     </div>
                 </div>
                 <div className="col-lg-3 col-md-4 col-sm-4">
                     <div class="form-check form-switch">
-                        <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault" />
+                        <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault" checked={this.state.isAssignedToDetails} onChange={this.onChangeAssignedToDetails} />
                         <label class="form-check-label" for="flexSwitchCheckDefault">Assigned to details</label>
                     </div>
                 </div>
@@ -155,19 +231,19 @@ function EditPreferences(props) {
             <div className="row">
                 <div className="col-lg-4 col-md-4 col-sm-4 spacing">
                     <div class="form-check form-switch">
-                        <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault" />
+                        <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault" checked={this.state.isCreatorDetails} onChange={this.onChangeCreatorDetails} />
                         <label class="form-check-label" for="flexSwitchCheckDefault">Creator Details</label>
                     </div>
                 </div>
                 <div className="col-lg-4 col-md-4 col-sm-4 spacing">
                     <div class="form-check form-switch">
-                        <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault" />
+                        <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault" checked={this.state.isCreationTime} onChange={this.onChangeCreationTime} />
                         <label class="form-check-label" for="flexSwitchCheckDefault">Creation Time</label>
                     </div>
                 </div>
                 <div className="col-lg-3 col-md-4 col-sm-4">
                     <div class="form-check form-switch">
-                        <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault" />
+                        <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault" checked={this.state.isQaContactDetails} onChange={this.onChangeQaContactDetails} />
                         <label class="form-check-label" for="flexSwitchCheckDefault">QA Contact details</label>
                     </div>
                 </div>
@@ -176,14 +252,22 @@ function EditPreferences(props) {
                 <br/>
                 <br/>
 
+                <br/>
+                <br/>
+                <br/>
+
             </div>
 
             <div className="row">
-                <div className="col-lg-6 col-md-6 col-sm-6"><button className="btn btn-sm submitbtndiv btn-secondary">Submit Preferences</button></div>
+                <div className="col-lg-8"></div>
+                <div className="col-lg-4 col-md-6 col-sm-6"><button className="btn btn-sm submitpreferencesbtn btn-secondary" onSubmit={this.onSubmit}>Submit Preferences</button></div>
             </div>
+
+            </form>
 
         </div>
     )
+    }
 
 }
 
